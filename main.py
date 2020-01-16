95e2052617bc16f3ecbf81da2b53548b2270bf36
AsabinaDaria <57720480+AsabinaDaria@users.noreply.github.com>
2020-01-16 19:49:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,31 +9,24 @@ from class_basic import *
 from class_gg import *
 from class_bullets import *
 from class_basic_bullets import *
-
-
+from class_camera import *
+from class_tile import *
+# всякая важная хрень
 pygame.init()
-screen = pygame.display.set_mode(size)
-gg_basic_bullets = BasicBullets([])
-gg = Gg(x_gg, y_gg, ['body1.0'], [gg_basic_bullets])
+camera = Camera()
 clock = pygame.time.Clock()
-# спрайты рождаются тут:
-# спрайты гг:
-gg_sprites = pygame.sprite.Group()
-gg_basic_bullet_sprites = pygame.sprite.Group()
-body1_image = load_image('body1.0.jpg', Color('White'))
-basic_bullet_image = load_image('basic_bullet.jpg', Color('White'))
+screen = pygame.display.set_mode(size)
 
-body_sprite = pygame.sprite.Sprite()
-body_sprite.image = body1_image
-body_sprite.rect = body_sprite.image.get_rect()
-gg_sprites.add(body_sprite)
+lvl = load_level('level.txt')
+player, width, height = generate_level(lvl)
+i = 0
+direction = None
+kind_of_pew = 'basic'
+hero_speed = 2
 
-basic_bullet_sprite = pygame.sprite.Sprite()
-basic_bullet_sprite.image = basic_bullet_image
-basic_bullet_sprite.rect = basic_bullet_sprite.image.get_rect()
-gg_basic_bullet_sprites.add(basic_bullet_sprite)
-
-# противника:
+pygame.mixer.music.load("Data/space_dreams_-_Space_Dreams.mp3")  #  нужно будет найти трек
+pygame.mixer.music.play()
+pewpew = pygame.mixer.Sound("Data/pew.wav")  #  выстрел
 
 while running:
     screen.fill((240, 240, 240))
@@ -41,41 +34,73 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == 119:
-                move_gg_up = True
-            if event.key == 115:
-                move_gg_down = True
-            if event.key == 97:
+            if event.key == 97 or event.key == 276:
                 move_gg_left = True
-            if event.key == 100:
+                player.dir_move(move_gg_up, move_gg_down,
+                                move_gg_left, move_gg_right)
+            if event.key == 100 or event.key == 275:
                 move_gg_right = True
+                player.dir_move(move_gg_up, move_gg_down,
+                                move_gg_left, move_gg_right)
+            if event.key == 119 or event.key == 273:
+                move_gg_up = True
+                player.dir_move(move_gg_up, move_gg_down,
+                                move_gg_left, move_gg_right)
+            if event.key == 115 or event.key == 274:
+                move_gg_down = True
+                player.dir_move(move_gg_up, move_gg_down,
+                                move_gg_left, move_gg_right)
             if event.key == 32:
-                gg.make_basic_pew()
+                player.make_pew(player.get_chars('kind_of_pew'), hero_pos, move_gg_up, move_gg_down,
+                                move_gg_left, move_gg_right)
+                pewpew.play()
+                # player.get_pos()
         if event.type == pygame.KEYUP:
-            if event.key == 119:
+            if event.key == 119 or event.key == 273:
                 move_gg_up = False
-            if event.key == 115:
+                player.dir_move(move_gg_up, move_gg_down,
+                                move_gg_left, move_gg_right)
+            if event.key == 115 or event.key == 274:
                 move_gg_down = False
-            if event.key == 97:
+                player.dir_move(move_gg_up, move_gg_down,
+                                move_gg_left, move_gg_right)
+            if event.key == 97 or event.key == 276:
                 move_gg_left = False
-            if event.key == 100:
+                player.dir_move(move_gg_up, move_gg_down,
+                                move_gg_left, move_gg_right)
+            if event.key == 100 or event.key == 275:
                 move_gg_right = False
+                player.dir_move(move_gg_up, move_gg_down,
+                                move_gg_left, move_gg_right)
 
-    # движение гг
-    gg.basic_move(move_gg_up, move_gg_down, move_gg_left, move_gg_right)
-    body_sprite.rect.x = gg.get_pos()[0]
-    body_sprite.rect.y = gg.get_pos()[1]
-    gg_sprites.draw(screen)
-
-    # движение пуль гг
-    num_of_basic_bullets = len(gg_basic_bullets.get_pos())
-    if num_of_basic_bullets:
-        for n in range(num_of_basic_bullets):
-            basic_bullet_sprite.rect.x = gg_basic_bullets.get_pos()[n][0]
-            basic_bullet_sprite.rect.y = gg_basic_bullets.get_pos()[n][1]
-            gg_basic_bullet_sprites.draw(screen)
-
+    if move_gg_down:
+        player.y_change(hero_speed)
+        player.rect.top += hero_speed
+    if move_gg_up:
+        player.y_change(-hero_speed)
+        player.rect.top -= hero_speed
+    if move_gg_right:
+        player.x_change(hero_speed)
+        player.rect.left += hero_speed
+    if move_gg_left:
+        player.x_change(-hero_speed)
+        player.rect.left -= hero_speed
+    # all_sprites.draw(screen)
+    tiles_group.draw(screen)
+    enemy_group.draw(screen)
+    player_group.draw(screen)
+    mels_group.draw(screen)
+    bullets_group.draw(screen)
+    camera.update(player)
+    for sprite in bullets_group:
+        sprite.update()
+    for sprite in enemy_group:
+        sprite.update()
+        sprite.follow(player)
+        # sprite.get_pos()
+    for sprite in all_sprites:
+        camera.apply(sprite)
     ###
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(fps)
     ###
