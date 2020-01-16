bbf72025c5a4f61f0d1c80c0fb4299b5bc16a4c2
AsabinaDaria <57720480+AsabinaDaria@users.noreply.github.com>
2020-01-16 19:43:43 +0300

    Update class_bullets.py

--- class_bullets.py
+++ class_bullets.py
@@ -3,12 +3,36 @@ from constants import *
 ### материнский класс пуль ###
 
 
-class Bullets():
-    def __init__(self, list_of_listes_of_poses_and_names=[]):
-        self.list_of_listes_of_poses_and_names = list_of_listes_of_poses_and_names
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y, direction, chars=[]):
+        super().__init__(all_sprites, bullets_group)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+        self.chars = chars
+        self.i = 0
+        self.move_up, self.move_down, self.move_left, self.move_right = direction
 
-    def make_pew(self, list_of_poses_and_names):
-        self.list_of_listes_of_poses_and_names.append(list_of_poses_and_names)
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
 
-    def get_pos(self):  # выводит координаты кортежем из двух циферок
-        return (self.list_of_listes_of_poses_and_names)
+    def update(self):
+        if self.move_up == True and self.move_right != True and self.move_left != True:
+            self.rect.top -= 2
+        if self.move_down == True and self.move_right != True and self.move_left != True:
+            self.rect.top += 2
+        if self.move_left == True:
+            self.rect.left -= 2
+        if self.move_right == True:
+            self.rect.left += 2
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
