678ed6321e2d8febb58cc5b8529cf8ca14600b72
AsabinaDaria <57720480+AsabinaDaria@users.noreply.github.com>
2020-01-16 19:48:42 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,7 +2,10 @@ import pygame
 from pygame.color import Color
 from pygame import *
 import os
-
+from constants import *
+from class_basic import *
+from class_gg import *
+from class_enemy import *
 #
 
 
@@ -17,3 +20,49 @@ def load_image(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.tile_images = {'wall': load_image(
+            'box.png'), 'empty': load_image('grass.png')}
+        self.image = self.tile_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Gg(load_image(
+                    "just_dote.jpg", Color('White')), 4, 1, hero_pos[0], hero_pos[1], ['basic'])
+                new_player.add(player_group)
+            elif level[y][x] == '!':
+                Tile('empty', x, y)
+                new_enemy = Enemy(load_image(
+                    "mar.png"), 1, 1, hero_pos[0] + 100, hero_pos[1] + 100, ['mel'])
+                new_enemy.add(enemy_group)
+
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
