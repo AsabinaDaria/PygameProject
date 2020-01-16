47d9d83d6e9ccefefbd18a9b52a38c9515b728ce
AsabinaDaria <57720480+AsabinaDaria@users.noreply.github.com>
2020-01-16 19:45:18 +0300

    Update class_enemy.py

--- class_enemy.py
+++ class_enemy.py
@@ -1,21 +1,59 @@
 from constants import *
 from class_basic import *
+from mel import *
 
 
 ### ну тут понятно я думаю ###
 
 
 class Enemy(Basic):
-    # отвечает за движение врага
-    def basic_move(self, move_gg_up, move_gg_down, move_gg_left, move_gg_right):
-        if move_gg_up:
-            self.pos_y -= 2
-        if move_gg_down:
-            self.pos_y += 2
-        if move_gg_left:
-            self.pos_x -= 2
-        if move_gg_right:
-            self.pos_x += 2
 
-    def update(self):
-        self.rect.left -= 2
+    def make_pew(self, kind_of_pew, pos):
+        if kind_of_pew == 'mel':
+            self.make_pew_mel(pos)
+
+    def follow(self, tar):
+        tar_x, tar_y = tar.get_pos()
+        if tar_y - 40 > self.y:
+            self.rect.top += 1
+            self.y_change(1)
+        elif tar_y + 40 < self.y:
+            self.rect.top -= 1
+            self.y_change(-1)
+        if tar_x - 40 > self.x:
+            self.rect.left += 1
+            self.x_change(1)
+        elif tar_x + 40 < self.x:
+            self.rect.left -= 1
+            self.x_change(-1)
+
+    def follow_and_hit(self, tar):
+        u1, u2 = True, True
+        tar_x, tar_y = tar.get_pos()
+        print(tar_x, tar_y)
+        print(self.x, self.y)
+        if tar_y - 40 > self.y:
+            self.rect.top += 1
+            self.y_change(1)
+            u1 = False
+        elif tar_y + 40 < self.y:
+            self.rect.top -= 1
+            self.y_change(-1)
+            u1 = False
+        # else:
+        #    u1 = True
+        if tar_x - 40 > self.x:
+            self.rect.left += 1
+            self.x_change(1)
+            u2 = False
+        elif tar_x + 40 < self.x:
+            self.rect.left -= 1
+            self.x_change(-1)
+            u2 = False
+        if u1 and u2:
+            self.make_pew(self.get_chars(
+                'kind_of_pew')[0], self.get_pos())
+
+    def make_pew_mel(self, pos):
+        new_bullet = Mel(load_image("mel.jpg",
+                                    Color('White')), 1, 1, pos[0], pos[1])
