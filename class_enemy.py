641daacbf2b5218a39fc569fe4647d0ae1efaa20
AsabinaDaria <57720480+AsabinaDaria@users.noreply.github.com>
2020-01-25 16:22:57 +0300

    score

--- class_enemy.py
+++ class_enemy.py
@@ -1,8 +1,8 @@
 from constants import *
 from class_basic import *
+from mel import *
 from pygame import *
 from heart import *
-from functions import *
 
 
 ### ну тут понятно я думаю ###
@@ -14,65 +14,62 @@ class Enemy(Basic):
         if kind_of_pew == 'mel':
             self.make_pew_mel(pos)
 
-    def follow(self, tar):  # ИИ врага
+    def follow(self, tar):
         if self.alive:
             tar_x, tar_y = tar.get_pos()
             if tar_y - 40 > self.y and abs(tar_y - self.y) < 200 and abs(tar_x - self.x) < 200:
                 self.rect.top += 1
                 self.y_change(1)
+
+                #self.rect.top += 1
+                # if pygame.sprite.spritecollideany(self, no_go_tiles_group):
+                #    self.rect.top -= 1
+                #    self.y_change(-1)
+                #self.rect.top -= 1
             elif tar_y + 40 < self.y and abs(tar_y - self.y) < 200 and abs(tar_x - self.x) < 200:
                 self.rect.top -= 1
                 self.y_change(-1)
+
+                #self.rect.top -= 1
+                # if pygame.sprite.spritecollideany(self, no_go_tiles_group):
+                #    self.rect.top += 1
+                #    self.y_change(+1)
+                #self.rect.top += 1
             if tar_x - 40 > self.x and abs(tar_x - self.x) < 200 and abs(tar_y - self.y) < 200:
                 self.rect.left += 1
                 self.x_change(1)
+
+                #self.rect.left += 1
+                # if pygame.sprite.spritecollideany(self, no_go_tiles_group):
+                #    self.rect.left -= 1
+                #    self.x_change(-1)
+                #self.rect.left -= 1
             elif tar_x + 40 < self.x and abs(tar_x - self.x) < 200 and abs(tar_y - self.y) < 200:
                 self.rect.left -= 1
                 self.x_change(-1)
 
-            if (tar_y - 40 <= self.y and tar_y + 40 >= self.y and  # атака врага
-                    tar_x - 40 <= self.x and tar_x + 40 >= self.x):
+                #self.rect.left -= 1
+                # if pygame.sprite.spritecollideany(self, no_go_tiles_group):
+                #    self.rect.left += 1
+                #    self.x_change(+1)
+                #self.rect.left += 1
+
+            if (tar_y - 40 <= self.y and tar_y + 40 >= self.y
+                    and tar_x - 40 <= self.x and tar_x + 40 >= self.x):
                 if self.tics + 500 < pygame.time.get_ticks():
                     self.tics = pygame.time.get_ticks()
-                    self.hit = pygame.mixer.Sound("Data/hit.wav")  # звук атаки
+                    self.hit = pygame.mixer.Sound("Data/hit.wav")
                     self.hit.play()
                     self.ht = self.counter_hp()
                     if self.alive:
                         self.heart = Heart(load_image("dead_heart.jpg", Color('white')), 1, 1,
-                                           self.ht, 0)  # убавление жизней
+                                       self.ht, 0)
 
     def counter_hp(self):
-        if self.sp:  # проверка на количество жизней гг
+        if self.sp:
             return self.sp.pop()
         self.alive = False
-        self.dead_screen()
-
-    def dead_screen(self):  # экран смерти
-        intro_text = ["You died :("]
-
-        fon = pygame.transform.scale(load_image('nothing.png'), (1500, 1000))
-        screen.blit(fon, (0, 0))
-        font = pygame.font.Font(None, 30)
-        text_coord = 20
-        for line in intro_text:
-            string_rendered = font.render(line, 2, pygame.Color('white'))
-            intro_rect = string_rendered.get_rect()
-            text_coord += 15
-            intro_rect.top = text_coord
-            intro_rect.x = 650
-            text_coord += intro_rect.height
-            screen.blit(string_rendered, intro_rect)
-
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    running = False
-                elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                    pygame.quit()
-                    running = False
-            pygame.display.flip()
-            clock.tick(60)
+        dead_screen()
 
     def follow_and_hit(self, tar):
         u1, u2 = True, True
@@ -104,11 +101,12 @@ class Enemy(Basic):
         new_bullet = Mel(load_image("mel.jpg",
                                     Color('White')), 1, 1, pos[0], pos[1])
 
-    def get_hitted(self):  # урон врага
-        self.robot_hit.play()
+    def get_hitted(self):
         self.chars[1] -= 1
         if self.chars[1] == 0:
-            self.kill()
+            self.rect.left -= 900
+            self.rect.top -= 900
+            self.alive = False
 
-    def is_alive(self):  # проверка на то, жив ли гг
+    def is_alive(self):
         return(self.alive)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 import pygame
 from pygame.color import Color
 from pygame import *
@@ -8,41 +7,38 @@ from heart import *
 from functions import *
 from constants import *
 from class_basic import *
+from class_gg import *
 from class_bullets import *
 from class_basic_bullets import *
 from class_camera import *
 from class_tile import *
-
-
+# всякая важная хрень
 pygame.init()
 camera = Camera()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode(size)
 
-level_list = 0, 1, 2, 3, 'boss'  
-lvl = load_level(f'level_{random.choice(level_list)}.txt')  # рандомный выбор уровня
-player, width, height = generate_level(lvl)
-mouse_pos = 0, 0  # для управления мышкой
+lvl = load_level('level.txt')
+player, no_go_dotes, width, height = generate_level(lvl)
 i = 0
 direction = None
 kind_of_pew = 'basic'
 hero_speed = 2
-score = 0
-control_type = 1  # для переключения управления в клавиатуры на мышку и обратно
+level_n = 1
 
-pygame.mixer.music.load("Data/soundtr.wav")  # фоновая музыка
+pygame.mixer.music.load("Data/soundtr.wav")
 pygame.mixer.music.play(loops=-1)
 pewpew = pygame.mixer.Sound("Data/pew.wav")  # выстрел
 
 
-start_screen()  # заставка
+start_screen()
 
 while running:
     screen.fill(Color('Black'))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN and control_type == 1:  # управление с клавиатуры
+        if event.type == pygame.KEYDOWN:
             if event.key == 97 or event.key == 276:
                 move_gg_left = True
                 player.dir_move(move_gg_up, move_gg_down,
@@ -60,7 +56,7 @@ while running:
                 player.dir_move(move_gg_up, move_gg_down,
                                 move_gg_left, move_gg_right)
 
-        if event.type == pygame.KEYUP and control_type == 1:
+        if event.type == pygame.KEYUP:
             if event.key == 119 or event.key == 273:
                 move_gg_up = False
                 player.dir_move(move_gg_up, move_gg_down,
@@ -78,42 +74,18 @@ while running:
                 player.dir_move(move_gg_up, move_gg_down,
                                 move_gg_left, move_gg_right)
 
-            if event.key == 32 and control_type == 1:  # выстрел
+
+            if event.key == 32:
                 pewpew.play()
                 player.make_pew(player.get_chars('kind_of_pew'), hero_pos, move_gg_up, move_gg_down,
                                 move_gg_left, move_gg_right)
 
-        if event.type == pygame.KEYDOWN :
-            if event.key == K_ESCAPE:  # пауза
+            if event.key == K_ESCAPE:
                 pause_screen()
-            if event.key == K_RETURN:  # смена управления
-                if control_type == 1:
-                    control_type = 2
-                else:
-                    control_type = 1
+            
+                # player.get_pos()
 
-        if event.type == pygame.MOUSEMOTION and control_type == 2:  # управление мышкой
-            mouse_pos = event.pos
-
-        if event.type == pygame.MOUSEBUTTONDOWN:  # выстрел
-            cur_frame = player.get_cur_frame()
-            pewpew.play()
-            if cur_frame == 0:
-                player.make_pew(player.get_chars('kind_of_pew'), hero_pos, False, True,
-                                False, False)
-            if cur_frame == 1:
-                player.make_pew(player.get_chars('kind_of_pew'), hero_pos, False, False,
-                                True, False)
-            if cur_frame == 2:
-                player.make_pew(player.get_chars('kind_of_pew'), hero_pos, False, False,
-                                False, True)
-            if cur_frame == 4:
-                player.make_pew(player.get_chars('kind_of_pew'), hero_pos, True, False,
-                                False, False)
-
-            # player.get_pos()
-
-    if move_gg_down:  # движение героя вниз
+    if move_gg_down:
         player.y_change(hero_speed)
         player.rect.top += hero_speed
 
@@ -122,7 +94,7 @@ while running:
             player.rect.top -= hero_speed
             player.y_change(-hero_speed)
         player.rect.top -= hero_speed
-    if move_gg_up:  # движение героя вверх
+    if move_gg_up:
         player.y_change(-hero_speed)
         player.rect.top -= hero_speed
 
@@ -131,7 +103,7 @@ while running:
             player.rect.top += hero_speed
             player.y_change(hero_speed)
         player.rect.top += hero_speed
-    if move_gg_right:  # движение героя вправо
+    if move_gg_right:
         player.x_change(hero_speed)
         player.rect.left += hero_speed
 
@@ -140,7 +112,7 @@ while running:
             player.rect.left -= hero_speed
             player.x_change(-hero_speed)
         player.rect.left -= hero_speed
-    if move_gg_left:  # движение героя влево
+    if move_gg_left:
         player.x_change(-hero_speed)
         player.rect.left -= hero_speed
 
@@ -149,50 +121,35 @@ while running:
             player.rect.left += hero_speed
             player.x_change(+hero_speed)
         player.rect.left += hero_speed
-    #  плюс проверка на наличие непроходимых спрайтов
-
     # all_sprites.draw(screen)
-    end_level_tiles_group.draw(screen)
     tiles_group.draw(screen)
     no_go_tiles_group.draw(screen)
-    items_group.draw(screen)
     enemy_group.draw(screen)
     player_group.draw(screen)
     mels_group.draw(screen)
     bullets_group.draw(screen)
     hearts_group.draw(screen)
     camera.update(player)
-    if control_type == 2:  # управление мышкой
-        player.follow_pos(mouse_pos, move_gg_up, move_gg_down,
-                          move_gg_left, move_gg_right)
-
     for sprite1 in bullets_group:
         for sprite2 in enemy_group:
             if pygame.sprite.collide_mask(sprite1, sprite2):
-                sprite1.kill()
+                sprite1.rect.left -= 1000
+                sprite1.rect.top -= 1000
+                sprite1.dead()
                 sprite2.get_hitted()
-                if not sprite2.is_alive():
-                    player.change_score(1)
     for sprite in bullets_group:
         sprite.update()
         if pygame.sprite.spritecollideany(sprite, no_go_tiles_group):
-            sprite.kill()
+            sprite.rect.left -= 1000
+            sprite.rect.top -= 1000
+            sprite.dead()
     for sprite in enemy_group:
         sprite.update_via_counter(30)
         sprite.follow(player)
-    for sprite in items_group:
-        if pygame.sprite.collide_mask(player, sprite):
-            sprite.used()
-    for sprite in end_level_tiles_group:
-        if pygame.sprite.collide_mask(player, sprite):
-            lvl = load_level(f'level_{random.choice(level_list)}.txt')
-            score += player.get_score()
-            for sprite in all_sprites:
-                sprite.kill()
-            player, width, height = generate_level(lvl)
         # sprite.get_pos()
     for sprite in all_sprites:
         camera.apply(sprite)
+
     end_level_flag = False
     end_level_counter = 0
     for enemy in enemy_group:
@@ -201,25 +158,10 @@ while running:
             end_level_counter -= 1
     if end_level_counter == 0:
         end_level_flag = True
-    if end_level_flag == True:
-        for sprite in end_level_tiles_group:
-            sprite.unlock_door()
-    # переходи между уровнями
-
-
-    intro_text = [f"Your score: {player.get_score() + score}"]
-    font = pygame.font.Font(None, 30)
-    text_coord = 20
-    for line in intro_text:
-        string_rendered = font.render(line, 2, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 5
-        intro_rect.top = text_coord
-        intro_rect.x = 1200
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-    # счет игрока
-
+    # if end_level_flag == True
+    # level_n += 1
+    # lvl = load_level('level.txt')
+    # player, width, height = generate_level(lvl)
     ###
     pygame.display.flip()
     clock.tick(fps)
